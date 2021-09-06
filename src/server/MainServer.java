333205a3c27aeb74f6b9e4936808670a1d4f1c02
pouya <pouyarezaei810@gmail.com>
2021-09-06 12:18:08 +0430

    create server app entry point

---
+++ src/server/MainServer.java
@@ -0,0 +1,10 @@
+package server;
+
+import java.io.IOException;
+
+
+public class MainServer {
+    public static void main(String[] args) throws IOException {
+        new Server().start();
+    }
+}
